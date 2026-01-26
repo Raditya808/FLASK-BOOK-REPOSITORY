c5fcc32c9b13efa3e6274b127de7ca7b47c019a3
Raditya808 <mraditradit808@gmail.com>
2026-01-26 08:04:07 +0700

    pemanggilan file menggunakan blueprint

---
+++ 07.Object request/main.py
@@ -0,0 +1,16 @@
+from flask import Flask,request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    headers = request.headers
+
+    response = [
+    '%s %s' % (key,value) for key,value in sorted(headers.items())
+    ]
+    response = '<br>'.join(response)
+    return response
+
+if __name__=="__main__":
+    app.run(debug=True)
